ffcd0f92c4403fb2a9138f2d14e54a83bef36afd
Brandon York <30988963+byork2005@users.noreply.github.com>
2017-11-03 16:13:41 -0500

    11/3 update

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,6 +12,9 @@ $(document).ready(function()
     firebase.initializeApp(config);
 
     var trainCount = 0;
+    // var mathHours;
+    // var mathMinutes;
+    // var totalMinutes;
 
     // Monitor for value changes. Change trainCount and runs makeRows fn.
     var database = firebase.database();
@@ -62,7 +65,7 @@ $(document).ready(function()
             var trainNameOutput = trainList[i].trainName;
             var destinationOutput = trainList[i].destination;
             var firstTrainOutput = trainList[i].firstTrain;
-            var frequencyOutput = trainList[i].frequency;
+            var frequencyOutput = parseInt(trainList[i].frequency);
 
             var newRow = $("<tr>");
 
@@ -78,36 +81,107 @@ $(document).ready(function()
             newRow.append(newNextArrival);
             newRow.append(newMinutesAway);
 
-            var time = (""+firstTrainOutput).split("");
-            console.log(time)
-
-
-
-
-
             $("tbody").append(newRow);
         }
     }
 
+    var nextArrivalOutput;
+    var minutesAwayOutput;
+
+
+    var string = moment("2300", "HHmm");
+    console.log(string);
+    var now = moment();
+    console.log(moment(now).isBefore(string, "m"));
+    var testFreq = 90;
+    var test = moment("1900", "HHmm").add(25, 'm');
+    console.log(test);
+
+    function nextArrival(first, freq)
+    {
+        moment(first).add(freq, "m");
+        if (moment(first).isBefore(now, "m"))
+        {
+            nextArrival(first, freq);
+        } else 
+        {
+            var nextArrivalOutput = first;
+            // var minutesAwayOutput = moment(first).toNow();
+        }
+        console.log(nextArrivalOutput);
+    }
+
+    // function nextArrival()
+    // {
+    //     moment("23:00").add(90, "m");
+    //     if (moment("23:00").isBefore(now, "m"))
+    //     {
+    //         nextArrival("23:00", 90);
+    //     } else 
+    //     {
+    //         var nextArrivalOutput = "23:00";
+    //         var minutesAwayOutput = moment("23:00").toNow;
+    //     }
+    // }
+    nextArrival(string, testFreq);
+    
+
+
+
+
+
+
+
+    // function stdTime(FTO)
+    // {
+    //     var firstTime = FTO.split(":");
+    //     var hours = Number(firstTime[0]);
+    //     var minutes = Number(firstTime[1]);
+    //     mathHours = Number(firstTime[0]);
+    //     mathMinutes = Number(firstTime[1]);
+    //     totalMinutes = (mathHours * 60 + mathMinutes)
+
+    //     var convertedStdTime;
+
+    //     if (hours > 0 && hours <= 12)
+    //     {
+    //       convertedStdTime= "" + hours;
+    //     } else if (hours > 12)
+    //     {
+    //         convertedStdTime= "" + (hours - 12);
+    //     }
+    //     else if (hours == 0)
+    //     {
+    //       convertedStdTime= "12";
+    //     }
+         
+    //     convertedStdTime += (minutes < 10) ? ":0" + minutes : ":" + minutes;
+    //     convertedStdTime += (hours >= 12) ? " P.M." : " A.M.";   
+    //     console.log(convertedStdTime)  
+    // }
+
+
+
+
     // function timeconverter(t) 
     // {
     //        
-    //         var minutes = Math.floor(t / 60);
-    //         var seconds = t - (minutes * 60);
+    //         var hours = Math.floor(t / 60);
+    //         var seconds = t - (hours * 60);
         
-    //         if (seconds < 10) {
-    //           seconds = "0" + seconds;
-    //         }
-        
-    //         if (minutes === 0) {
-    //           minutes = "00";
-    //         }
-    //         else if (minutes < 10) {
+    //         if (minutes < 10) {
     //           minutes = "0" + minutes;
+    //         }
+        
+    //         if (hours === 0) {
+    //           hours = "00";
+    //         }
+    //         else if (hours < 10) {
+    //           hours = "0" + hours;
     //         }
             
         
-    //         minutes + ":" + seconds;
+    //        hours + ":" + minutes;
     // };
 
         
